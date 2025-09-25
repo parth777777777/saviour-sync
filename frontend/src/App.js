397cf122d67fd559316209d4e591ccfc28899e5e
vamshialle66 <vamshialle66@gmail.com>
2025-09-25 21:13:49 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,32 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import HomePage from "./pages/HomePage";
+// Pages
+import Home from "./pages/Home";
 import RegisterPage from "./pages/RegisterPage";
 import SearchPage from "./pages/SearchPage";
 import AboutPage from "./pages/AboutPage";
-import DonorListPage from "./pages/DonorListPage";
 import ContactPage from "./pages/ContactPage";
+import LoginPage from "./pages/LoginPage"; // Single login page for both user/admin
+
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        <Header />
+        <Header /> {/* Header component */}
         <main className="flex-grow">
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<Home />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/search" element={<SearchPage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/contact" element={<ContactPage />} />
-            <Route path="/donors" element={<DonorListPage />} /> {/* ← new route */}
+            <Route path="/login" element={<LoginPage />} /> {/* Single login page */}
+            
           </Routes>
         </main>
-        <Footer />
+        <Footer /> {/* Footer component */}
       </div>
     </Router>
   );
